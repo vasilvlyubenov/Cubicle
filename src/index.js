c1130f2695cb7f5dc84fb7893d0a1db467ce59c7
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-05-23 20:20:20 +0300

    Add handlebars settings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const PORT = 5000;
 
+// Handlebars setup
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.render('index');
 });
 
 app.listen(PORT, () => { console.log(`Server is running pm port ${PORT}...`) });

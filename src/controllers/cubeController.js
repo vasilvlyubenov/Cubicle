f27ff6c7d70a7c2e95e3d318a7d25d9c3f0a42f2
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-05-24 16:39:36 +0300

    Saving data in array with uniqe id

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
+const cubeService = require('../services/cubeService');
 
 router.get('/create', (req, res) => {
+    console.log(cubeService.getAll());
     res.render('create');
 });
 
 router.post('/create', (req, res) => {
-    console.log(req.body);
+    const { name, description, imageUrl, difficultyLevel } = req.body;
 
+    cubeService.create({ name, description, imageUrl, difficultyLevel: Number(difficultyLevel) });
     res.redirect('/');
 });
 
---
+++ src/services/cubeService.js
@@ -0,0 +1,14 @@
+const uniqid = require('uniqid');
+const cubes = [];
+
+exports.getAll = () => cubes.slice();
+
+exports.create = (cubeData) => {
+    const newCube = {
+        id: uniqid(),
+        ...cubeData,
+    };
+
+    cubes.push(newCube);
+    return newCube;
+};

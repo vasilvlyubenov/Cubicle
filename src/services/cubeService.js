7e2916b6fb7818159fb353ca965e513df37486da
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-06-04 14:59:31 +0300

    add create accessory page

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,20 +1,5 @@
 const Cube = require('../models/Cube');
 
-// const cubes = [
-//     {
-//         id: 'iwaf3ywli1re4mb',
-//         name: 'Rubik\'s cube',
-//         imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUGI9F4FUClgI6CPofz8ru2lSoGOueBJPW2Q&usqp=CAU',
-//         difficultyLevel: 3
-//     },
-//     {
-//         id: 'iwaf46oli1rc628',
-//         name: 'Rubik\'s cube Jigsaw',
-//         imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB_5sx4nmi6GgzTJ-HHnaSFTzWma3xB33iDg&usqp=CAU',
-//         difficultyLevel: 6
-//     },
-// ];
-
 exports.getAll = async (search, from, to) => {
     let result = await Cube.find().lean();
 

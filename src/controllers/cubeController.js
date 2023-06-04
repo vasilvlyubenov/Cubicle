b610d5a868236bca3c5d72d61182623bfad545e1
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-06-04 15:24:57 +0300

    add attach accessory link

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -23,4 +23,8 @@ router.post('/create', async (req, res) => {
     res.redirect('/');
 });
 
+router.get('/:cubeId/attach-accessory', (req, res) => {
+    res.render('accessory/attach');
+});
+
 module.exports = router;

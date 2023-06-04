111d96dbb0e322b363bde929a880207971dbcc5f
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-06-04 13:46:54 +0300

    db setup

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ const express = require('express');
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 const routes = require('./routes');
+const dbConnect = require('./config/dbConfig');
 
 const app = express();
 const PORT = 5000;
 
+dbConnect()
+    .then(() => console.log('DB Connected successfully.'))
+    .catch(err => {
+        console.log('DB error:', err);
+    });
 expressConfig(app);
 handlebarsConfig(app);
 
 app.use(routes);
 
-app.listen(PORT, () => { console.log(`Server is running pm port ${PORT}...`); });
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}...`); });

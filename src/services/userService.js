39846bfa0292924589ee020503138b759ecbc859
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-06-11 18:12:48 +0300

    added login

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,22 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../models/User');
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (username, password) => {
+    //TODO find user
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new Error('Cannot find username of password.');
+    }
+    //TODO validate password
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Cannot find username of password.');
+    }
+    
+    return user;
+};

7096b57b43a3118c61252edf020c50b75bd8d5fe
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-06-11 17:11:14 +0300

    save user and validated password in db

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
+const userService = require('../services/userService');
+
 router.get('/register', (req, res) => {
     res.render('users/register');
+});
+
+router.post('/register', async (req, res) => {
+    const { username, password, repeatPassword } = req.body;
+
+    await userService.register({ username, password, repeatPassword });
+
+    res.redirect('login');
+});
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
 });
 
 module.exports = router;

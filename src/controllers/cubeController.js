a261795a056b7e9d533b881f42f8b911ac2765f0
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-06-11 19:15:22 +0300

    added cube owner

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -21,7 +21,7 @@ router.get('/details/:cubeId', async (req, res) => {
 router.post('/create', async (req, res) => {
     const { name, description, imageUrl, difficultyLevel } = req.body;
 
-    await cubeService.create({ name, description, imageUrl, difficultyLevel: Number(difficultyLevel) });
+    await cubeService.create({ name, description, imageUrl, difficultyLevel: Number(difficultyLevel), owner: req.user._id });
     res.redirect('/');
 });
 

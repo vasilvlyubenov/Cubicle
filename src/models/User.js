0f5331d877d297e0f2f750d786ad8bd92549bc89
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2023-06-11 17:46:05 +0300

    hashing user password before save in db

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: String,
@@ -18,6 +19,12 @@ userSchema.virtual('repeatPassword')
         if (value !== this.password) {
             throw new mongoose.MongooseError('Password missmatch!');
         }
+    });
+
+    userSchema.pre('save', async function() {
+        const hash = await bcrypt.hash(this.password, 10);
+
+        this.password = hash;
     });
 
 const User = mongoose.model('User', userSchema);
